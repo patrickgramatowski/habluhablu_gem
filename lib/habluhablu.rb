c26222c239681f35b795fbe7e9adbf7c728f4d89
patrick <patrick.gramatowski@gmail.com>
2021-05-05 15:09:30 +0200

    .hablu method added

--- lib/habluhablu.rb
+++ lib/habluhablu.rb
@@ -2,7 +2,15 @@
 
 require_relative "habluhablu/version"
 
+# Module HabluHablu
 module Habluhablu
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.hablu(language)
+    File.open("./config/locales/es.yml", "a+") do |f|
+      File.open("./languages/#{language}.yml") do |i18n|
+        f.write(i18n.read.to_s)
+      end
+    end
+  end
 end
