c141c1ffc9664d929e05ecec7a28e140a5bab6bd
patrick <patrick.gramatowski@gmail.com>
2021-05-05 21:52:40 +0200

    specs update

--- spec/habluhablu_spec.rb
+++ spec/habluhablu_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "fileutils"
+
 RSpec.describe Habluhablu do
   it "has a version number" do
     expect(Habluhablu::VERSION).not_to be nil
@@ -12,9 +14,23 @@ RSpec.describe Habluhablu do
         expect(directory).to eq true
       end
     end
+
+    context "when I18n gem is not included" do
+      let(:language) { "es" }
+      let(:create_file) { described_class.hablu(language) }
+      let!(:directory) { FileUtils.rm_rf("config") }
+      it "has not ./config/locales directory" do
+        expect { create_file }.to raise_error(Habluhablu::GemI18nError)
+      end
+    end
   end
 
   describe ".hablu" do
+    before(:all) do
+      Dir.mkdir("config")
+      Dir.mkdir("config/locales")
+    end
+
     context "when language specified" do
       let!(:create_file) { described_class.hablu("es") }
       it "creates a file" do
@@ -40,6 +56,14 @@ RSpec.describe Habluhablu do
       let(:content) { File.open("./languages/iw.yml", &:read) }
       it "returns basic phrases in valid I18n syntax" do
         expect(File.read("./config/locales/iw.yml")).to match(content)
+      end
+    end
+
+    context "when specified language's symbol does not exist" do
+      let(:language) { "wrong" }
+      let(:wrong_language) { described_class.hablu(language) }
+      it "throws an error" do
+        expect { wrong_language }.to raise_error(Habluhablu::WrongSymbolError)
       end
     end
   end
