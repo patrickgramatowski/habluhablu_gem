79b558ea842171dcd6f3749f31cd57cfeffa27bc
patrick <patrick.gramatowski@gmail.com>
2021-05-05 15:37:13 +0200

    hablu method update

--- lib/habluhablu.rb
+++ lib/habluhablu.rb
@@ -7,7 +7,7 @@ module Habluhablu
   class Error < StandardError; end
 
   def self.hablu(language)
-    File.open("./config/locales/es.yml", "a+") do |f|
+    File.open("./config/locales/#{language}.yml", "a+") do |f|
       File.open("./languages/#{language}.yml") do |i18n|
         f.write(i18n.read.to_s)
       end
