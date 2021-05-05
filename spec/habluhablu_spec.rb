be444620483f8af8f41ecbddd3d0c4d0f91c1d39
patrick <patrick.gramatowski@gmail.com>
2021-05-05 15:09:52 +0200

    .hablu_spec method tested

--- spec/habluhablu_spec.rb
+++ spec/habluhablu_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe Habluhablu do
   it "has a version number" do
     expect(Habluhablu::VERSION).not_to be nil
   end
+
+  describe ".hablu" do
+    context "when language specified" do
+      let!(:create_file) { described_class.hablu("es") }
+      it "creates a file" do
+        expect(File.exist?("./config/locales/es.yml")).to eq true
+      end
+    end
+
+    context "when file has been created" do
+      let(:content) { File.open("./languages/es.yml", &:read) }
+      it "returns basic phrases in valid I18n syntax" do
+        expect(File.read("./config/locales/es.yml")).to match(content)
+      end
+    end
+  end
 end
