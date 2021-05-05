91613312b715474ab713d2633a4fac9857516941
patrick <patrick.gramatowski@gmail.com>
2021-05-05 15:37:27 +0200

    specs update

--- spec/habluhablu_spec.rb
+++ spec/habluhablu_spec.rb
@@ -5,6 +5,15 @@ RSpec.describe Habluhablu do
     expect(Habluhablu::VERSION).not_to be nil
   end
 
+  describe "I18n gem" do
+    context "when I18n gem included" do
+      let(:directory) { Dir.exist?("./config/locales") }
+      it "has ./config/locales directory" do
+        expect(directory).to eq true
+      end
+    end
+  end
+
   describe ".hablu" do
     context "when language specified" do
       let!(:create_file) { described_class.hablu("es") }
@@ -17,6 +26,20 @@ RSpec.describe Habluhablu do
       let(:content) { File.open("./languages/es.yml", &:read) }
       it "returns basic phrases in valid I18n syntax" do
         expect(File.read("./config/locales/es.yml")).to match(content)
+      end
+    end
+
+    context "when other language specified" do
+      let!(:create_other_file) { described_class.hablu("iw") }
+      it "creates a file" do
+        expect(File.exist?("./config/locales/iw.yml")).to eq true
+      end
+    end
+
+    context "when other file has been created" do
+      let(:content) { File.open("./languages/iw.yml", &:read) }
+      it "returns basic phrases in valid I18n syntax" do
+        expect(File.read("./config/locales/iw.yml")).to match(content)
       end
     end
   end
