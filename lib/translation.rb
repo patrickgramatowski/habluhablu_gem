9e05adb76a1b083f1734fbb93d10744b3bcab48d
patrick <patrick.gramatowski@gmail.com>
2021-05-12 10:53:55 +0200

    version-0.4.0 update

---
+++ lib/translation.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require "uri"
+require "net/http"
+require "openssl"
+
+# Class comment
+class Translation
+  def initialize(text, target, source = "en")
+    @text = text
+    @target = target
+    @source = source
+    @url = URI("https://google-translate1.p.rapidapi.com/language/translate/v2")
+
+    @http = Net::HTTP.new(@url.host, @url.port)
+    @http.use_ssl = true
+    @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    @request = Net::HTTP::Post.new(@url)
+    @request["content-type"] = "application/x-www-form-urlencoded"
+    @request["accept-encoding"] = "application/gzip"
+    File.open("config/api_key.txt", "a+") do |f|
+      $KEY = f.read
+    end
+    if $KEY.nil?
+      @request["x-rapidapi-key"] = "069f34f2bamsha3ef70a3f7e20cap1dcfe9jsn2dcff8d20c4e"
+    else
+      @request["x-rapidapi-key"] = $KEY
+    end
+    @request["x-rapidapi-host"] = "google-translate1.p.rapidapi.com"
+    @request.body = @text.gsub(" ", "%2C%20") + "!&target=#{@target}&source=#{@source}"
+    # Example "q=Hello%2C%20dear%2C%20friend!&target=es&source=en"
+  end
+
+  def response
+    response = @http.request(@request)
+    response = response.read_body[:data][:translations].first[:translatedText]
+    File.open("config/locales/#{@target}", "a+") do |f|
+      f.write({ "translation" => response }.to_yaml)
+    end
+  end
+end
